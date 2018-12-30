688a8d241e84ac6016bb6cbac804de9b919ed4a0
Shoaib Doulatabad <shoaib.doulatabad@gmail.com>
2018-12-30 21:24:28 +0400

    updated helloworld from git

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld{
 
 public static void main(String args[]){
 
-	System.out.println("Hello World! from jenkins");
+	System.out.println("Hello World! From Github to jenkins");
 
 }
 
